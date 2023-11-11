e06e5af7f7cb18d75420aeefd1818b5d4dc99d15
singgihseptiann <septiansinggih93@gmail.com>
2023-11-12 00:30:02 +0700

    remove console

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Container, Row, Col, Form, Button } from "react-bootstrap";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { registerAuth } from "../../store/auth";
+import { registerAuth } from "../../Store/auth";
 import { useDispatch } from "react-redux";
 import { Link } from "react-router-dom";
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
@@ -20,10 +20,13 @@ const LoginPage = () => {
     e.preventDefault();
     setLoad(true);
     try {
-      const res = await axios.post("https://api-car-rental.binaracademy.org/customer/auth/login", {
-        email: form.email,
-        password: form.password,
-      });
+      const res = await axios.post(
+        "https://api-car-rental.binaracademy.org/customer/auth/login",
+        {
+          email: form.email,
+          password: form.password,
+        }
+      );
 
       if (res.status === 201) {
         setSuccess("Login Successfully");
@@ -89,17 +92,37 @@ const LoginPage = () => {
                       <Form.Label as="label" htmlFor="email">
                         Email
                       </Form.Label>
-                      <Form.Control type="email" placeholder="Example: johndee@gmail.com" name="email" value={form.email} onChange={handleChange} />
+                      <Form.Control
+                        type="email"
+                        placeholder="Example: johndee@gmail.com"
+                        name="email"
+                        value={form.email}
+                        onChange={handleChange}
+                      />
                     </Form.Group>
                     {/* controlId="password" */}
                     <Form.Group className="mb-4">
                       <Form.Label as="label" htmlFor="password">
                         Password
                       </Form.Label>
-                      <Form.Control type={showPassword ? "text" : "password"} placeholder="6+ characters" name="password" value={form.password} onChange={handleChange} />
+                      <Form.Control
+                        type={showPassword ? "text" : "password"}
+                        placeholder="6+ characters"
+                        name="password"
+                        value={form.password}
+                        onChange={handleChange}
+                      />
                       <div className="d-flex flex-row justify-space-between align-items-center">
-                        <Button variant="link" className="password-toggle" onClick={togglePasswordVisibility}>
-                          {showPassword ? <AiFillEyeInvisible /> : <AiFillEye />}
+                        <Button
+                          variant="link"
+                          className="password-toggle"
+                          onClick={togglePasswordVisibility}
+                        >
+                          {showPassword ? (
+                            <AiFillEyeInvisible />
+                          ) : (
+                            <AiFillEye />
+                          )}
                         </Button>
                         <div>Show Password</div>
                       </div>
@@ -112,12 +135,17 @@ const LoginPage = () => {
                 {success && <p className="text-success">{success}</p>}
                 {error && <p className="text-danger">{error}</p>}
                 <p className="text-center mt-4">
-                  Don’t have an account? <Link to="/signup">Sign Up For Free</Link>
+                  Don’t have an account?{" "}
+                  <Link to="/signup">Sign Up For Free</Link>
                 </p>
               </div>
             </Col>
             <Col className="col-md-6 d-none d-md-block">
-              <img src={`${process.env.PUBLIC_URL}/images/login.png`} alt="Login" className="login-img img-fluid" />
+              <img
+                src={`${process.env.PUBLIC_URL}/images/login.png`}
+                alt="Login"
+                className="login-img img-fluid"
+              />
             </Col>
           </Row>
         </Container>
